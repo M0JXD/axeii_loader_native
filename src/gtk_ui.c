56b7133f770891872b731747a830946bbe4278ef
M0JXD <jamiedrinkell@gmail.com>
2026-01-30 00:15:08 +0000

    Simplify UI test program and format as I like

--- src/gtk_ui.c
+++ src/gtk_ui.c
@@ -1,38 +1,26 @@
 #include <gtk/gtk.h>
 
-static void
-print_hello (GtkWidget *widget,
-             gpointer   data)
-{
-  g_print ("Hello World\n");
-}
+int main(int argc, char *argv[]) {
+    GtkBuilder *builder;
+    GObject *window;
+    GObject *button;
+    GError *error = NULL;
 
-int
-main (int   argc,
-      char *argv[])
-{
-  GtkBuilder *builder;
-  GObject *window;
-  GObject *button;
-  GError *error = NULL;
+    gtk_init (&argc, &argv);
 
-  gtk_init (&argc, &argv);
-
-  /* Construct a GtkBuilder instance and load our UI description */
-  builder = gtk_builder_new ();
-  if (gtk_builder_add_from_file (builder, "gtk_ui.ui", &error) == 0)
-    {
-      g_printerr ("Error loading file: %s\n", error->message);
-      g_clear_error (&error);
-      return 1;
+    builder = gtk_builder_new ();
+    if (gtk_builder_add_from_file(builder, "gtk_ui.ui", &error) == 0) {
+        g_printerr("Error loading file: %s\n", error->message);
+        g_clear_error(&error);
+        return 1;
     }
 
-	  /* Connect signal handlers to the constructed widgets. */
-  window = gtk_builder_get_object (builder, "window");
-  g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
+    /* Connect signal handlers to the constructed widgets. */
+    window = gtk_builder_get_object(builder, "window");
+    g_signal_connect(window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
 
 
-  gtk_main ();
+    gtk_main();
 
-  return 0;
+    return 0;
 }
