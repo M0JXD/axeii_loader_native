51d61bf3776779680f693191bd0cae771a80c0ce
M0JXD <jamiedrinkell@gmail.com>
2026-02-01 14:05:05 +0000

    Untested incomplete OSS backend

--- src/oss/axeii_oss.c
+++ src/oss/axeii_oss.c
@@ -15,31 +15,78 @@
  *    with this program; if not, see <https://www.gnu.org/licenses/>.
  */
 
+#include <stdio.h>
+/*#include <fcntl.h>*/
+#include <sys/soundcard.h>
+
 #include "../midi_interface.h"
+
+/* GLOBAL */
+
+static int input, output;
 
 /* FUNCTIONS */
 
 char initMIDI(char *devString) {
+    if ((input = open(devString, O_RDONLY, 0)) == -1) {
+        perror("Open Input MIDI Error!");
+        return -1;
+    }
+
+    if ((output = open(devString, O_WRONLY, 0)) == -1) {
+        perror("Open Output MIDI Error!");
+        return -1;
+    }
     return 0;
 }
 
 char closeMIDI(void) {
+    close(input);
+    close(output);
     return 0;
 }
 
 char sendMidi(unsigned char *data, unsigned int len) {
+    if (write(output, data, len) == -1) {
+        perror("Send MIDI Error!");
+        return -1;
+    }
+
     return 0;
 }
 
 char getMidi(unsigned char *data, unsigned int len) {
+    if (read(input, data, len) == -1) {
+        perror("Read MIDI Error!");
+        return -1;
+    }
     return 0;
 }
 
 char clearMidiInBuffer(void) {
+    int ret;
+    struct pollfd fds = {
+        fd = input;  /* file descriptor */
+        events = POLLIN | POLLRDNORM | POLLRDBAND | POLLPRI;   /* requested events */
+        revents = 1; /* returned events */
+    };
+
+    while (1) {
+        ret = poll(&fds, 1, 2);
+        if (ret = 0) {
+            /* Timed out */
+            break;
+        } else if (ret > 0) {
+            /* Throwaway until it does timeout */
+            char throwaway;
+            read(input, &throwaway, 1);
+        }
+    }
     return 0;
 }
 
 char clearMidiOutBuffer(void) {
+    /* OSS buffers everything and it will sync eventually, nothing to do */
     return 0;
 }
 
